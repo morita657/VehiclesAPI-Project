152b2865c6ed119769572fb73b42e6edf612b6f0
morita657 <morita657@gmail.com>
2020-09-07 12:33:35 +0900

    Initial commit

---
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
@@ -0,0 +1,10 @@
+package com.udacity.pricing.domain.price;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import com.udacity.pricing.domain.price.Price;
+
+//@Repository
+public interface PriceRepository extends CrudRepository<Price, Long> {
+
+}
