ee9753655536835732f66b4dd59c1f7440fb5b4f
morita657 <morita657@gmail.com>
2020-09-14 20:34:45 +0900

    Remove todos

--- vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
@@ -40,17 +40,9 @@ public class CarService {
      * @return the requested car's information, including location and price
      */
     public Car findById(Long id) {
-        /**
-         * TODO: Find the car by ID from the `repository` if it exists.
-         *   If it does not exist, throw a CarNotFoundException
-         *   Remove the below code as part of your implementation.
-         */
         Car car = repository.findById(id).orElseThrow(CarNotFoundException::new);
 
         /**
-         * TODO: Use the Pricing Web client you create in `VehiclesApiApplication`
-         *   to get the price based on the `id` input'
-         * TODO: Set the price of the car
          * Note: The car class file uses @transient, meaning you will need to call
          *   the pricing service each time to get the price.
          */
@@ -58,10 +50,6 @@ public class CarService {
 
 
         /**
-         * TODO: Use the Maps Web client you create in `VehiclesApiApplication`
-         *   to get the address for the vehicle. You should access the location
-         *   from the car object and feed it to the Maps service.
-         * TODO: Set the location of the vehicle, including the address information
          * Note: The Location class file also uses @transient for the address,
          * meaning the Maps service needs to be called each time for the address.
          */
