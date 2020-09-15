70714ce0329912211b642b64ba099bbb5c2c6832
morita657 <morita657@gmail.com>
2020-09-15 15:13:28 +0900

    Implement pricing feature

--- pricing-service/src/main/java/com/udacity/pricing/domain/Price.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/Price.java
@@ -1,3 +1,50 @@
-package com.udacity.pricing;
+package com.udacity.pricing.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
 public class Price {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String currency;
+    private BigDecimal price;
+    private Long vehicleId;
+
+    public Price() {
+    }
+
+    public Price(String currency, BigDecimal price, Long vehicleId) {
+        this.currency = currency;
+        this.price = price;
+        this.vehicleId = vehicleId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Long getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(Long vehicleId) {
+        this.vehicleId = vehicleId;
+    }
 }
